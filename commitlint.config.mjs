c7dac167e982d047a886b3e32917d6b873f4490b
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-09-11 07:35:06 +0200

    chore: add standard commitlint config

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,9 +1,27 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-max-length': [1, 'always', 120],
-    'body-max-line-length': [0, 'always'],
-    'footer-max-line-length': [0, 'always'],
-    'subject-case': [0, 'always'],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "perf",
+        "docs",
+        "chore",
+        "refactor",
+        "style",
+        "test",
+        "ci",
+        "build",
+        "deps",
+      ],
+    ],
+    "header-max-length": [1, "always", 120],
+    "body-max-line-length": [0, "always"],
+    "footer-max-line-length": [0, "always"],
+    "subject-case": [0, "always"],
   },
+  ignores: [(message) => message.includes("renovate[bot]")],
 };
